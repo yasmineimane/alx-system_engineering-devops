b39b85dba2c883c33a0d260db972a09e181a8f52
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2024-07-23 13:18:09 -0700

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,17 @@
 # 1-install_a_package.pp
 
 package { 'python3.8':
- ensure   => '3.8.10',
- provider => 'pip3',
+ensure   => '3.8.10',
+provider => 'pip3',
 }
 
 package { 'Flask':
- ensure   => '2.1.0',
- provider => 'pip3',
+ensure   => '2.1.0',
+provider => 'pip3',
 }
 
 package { 'Werkzeug':
- ensure   => '2.1.1',
- provider => 'pip3',
- require  => Package['Flask'],
+ensure   => '2.1.1',
+provider => 'pip3',
+require  => Package['Flask'],
 }

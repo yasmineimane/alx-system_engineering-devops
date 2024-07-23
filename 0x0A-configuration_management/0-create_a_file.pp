fcd3ffce47791d9ea9b64bf2d1be5aad91a323e4
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2024-07-23 09:32:03 -0700

    Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#creating a file in /tmp using puppet
+
+file { '/tmp/school':
+ensure  => 'file',
+content => 'I love Puppet',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+}
